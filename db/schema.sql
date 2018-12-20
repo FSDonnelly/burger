0928214210396d3acced37acee718b4a857ea068
FSDonnelly <donnelly_frank@yahoo.com>
2018-12-19 19:15:58 -0800

    finished orm.js, connection.js, burger_controller.js,seed.sql, burger.js, and some stuff on server.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,16 @@
 -- Drops the favorite_db if it exists currently --
 DROP DATABASE IF EXISTS burgers_db;
--- Creates the "favorite_db" database --
+-- Creates the "burgers_db" database --
 CREATE DATABASE burgers_db;
 
--- Makes it so all of the following code will affect favorite_db --
+-- Makes it so all of the following code will affect burgers_db --
 USE burgers_db;
 
--- Creates the table "favorite_foods" within favorite_db --
+-- Creates the table "favorite_foods" within burgers_db --
 CREATE TABLE burgers  (
    id INTEGER NOT NULL AUTO_INCREMENT,
    burger_name VARCHAR(50)NOT NULL,
-   devoured BOOLEAN
+   devoured BOOLEAN DEFAULT FALSE,
+   PRIMARY KEY(id)
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Western Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double Animal Style', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggi Burger');
